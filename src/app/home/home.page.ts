8ed09c2af2f078d6c18ff2536d8043c3d710351a
Luis Fernando Nestares Valverde <68039881+luisfernando1901@users.noreply.github.com>
2021-05-11 23:17:12 -0500

    first deploy

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  usuario:string;
+  message: string;
+  @ViewChild("divMessages", { read: ElementRef }) private divMessages: ElementRef;
+  constructor(private renderer: Renderer2,
+    private http: HttpClient,
+    private socket: Socket) {
+    //http.get('http://localhost:3000/').subscribe(params => console.log(params));
+    http.get('https://thawing-waters-69626.herokuapp.com/').subscribe(params => console.log(params));
+    socket.connect();
+    socket.emit("setUsuario",this.usuario);
+    socket.on("message", (param: object) => {
+      //create the DOM element 
+      let p: HTMLParagraphElement = this.renderer.createElement('p');
+      //create text for the element
+      p.innerHTML = `<b>${param['usuario']} dice</b>: <br> ${param['mssg']}`;
+      //Now append the li tag to divMessages div
+      this.renderer.appendChild(this.divMessages.nativeElement, p);
+    })
+  }
 
-  constructor() {}
+  sendMessage() {
+    this.socket.emit("setUsuario",this.usuario);
+    this.socket.emit("message",this.message);
+    //create the DOM element 
+    let p: HTMLParagraphElement = this.renderer.createElement('p');
+    //create text for the element
+    p.innerHTML = `<b>${this.usuario} dice</b>: <br> ${this.message}`;
+    //Now append the li tag to divMessages div
+    this.renderer.appendChild(this.divMessages.nativeElement, p);
+    console.log(this.message);
+    this.message = '';
+  }
 
 }
